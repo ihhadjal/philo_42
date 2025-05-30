f2f380632c12869d963b884bcc0bd9e3201420e4
ihhdj <ihebhdj42@gmail.com>
2025-05-30 13:23:41 +0200

    started to init args

--- code/utils.c
+++ code/utils.c
@@ -6,12 +6,11 @@
 /*   By: ihhadjal <ihhadjal@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/28 15:50:02 by ihhadjal          #+#    #+#             */
-/*   Updated: 2025/05/30 11:26:56 by ihhadjal         ###   ########.fr       */
+/*   Updated: 2025/05/30 11:51:24 by ihhadjal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../resources/philo.h"
-
 
 int	parsing(int argc, char **argv)
 {
@@ -23,14 +22,16 @@ int	parsing(int argc, char **argv)
 		return (printf("error: the ammount of arguments is not right\n"), 1);
 	while (argv[i])
 	{
-		num = ft_atoi(argv[i]);
+		num = my_atoi(argv[i]);
 		check_input(argv[i]);
 		if (num <= 0)
-			return(printf("error: all arguments must be higher than zero\n"), 1);
+			return (printf("error: all arguments must be higher than zero\n"),
+				1);
 		i++;
 	}
 	return (0);
 }
+
 int	check_input(char *argv)
 {
 	int	i;
@@ -38,7 +39,7 @@ int	check_input(char *argv)
 	i = 0;
 	while (argv[i])
 	{
-		if (!ft_isdigit(argv[i]))
+		if (!my_isdigit(argv[i]))
 		{
 			printf("error: only numbers are accepted\n");
 			exit(1);
@@ -46,4 +47,34 @@ int	check_input(char *argv)
 		i++;
 	}
 	return (0);
+}
+
+int	my_atoi(const char *nptr)
+{
+	int	i;
+	int	res;
+	int	sign;
+
+	res = 0;
+	sign = 1;
+	i = 0;
+	while ((nptr[i] >= 9 && nptr[i] <= 13) || nptr[i] == ' ')
+		i++;
+	if (nptr[i] == '-' || nptr[i] == '+')
+	{
+		if (nptr[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	while (nptr[i] >= '0' && nptr[i] <= '9')
+	{
+		res = res * 10 + (nptr[i] - '0');
+		i++;
+	}
+	return (res * sign);
+}
+
+int	my_isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
 }
--- resources/philo.h
+++ resources/philo.h
@@ -6,7 +6,7 @@
 /*   By: ihhadjal <ihhadjal@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/08 12:16:45 by ihhadjal          #+#    #+#             */
-/*   Updated: 2025/05/30 11:26:14 by ihhadjal         ###   ########.fr       */
+/*   Updated: 2025/05/30 13:16:27 by ihhadjal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,43 @@
 # include <stdlib.h>
 # include <unistd.h>
 
-int	parsing(int argc, char **argv);
-int	check_input(char *argv);
+typedef struct s_arguments
+{
+	int				nb_philo;
+	int				time_die;
+	int				time_eat;
+	int				time_sleep;
+}					t_arguments;
+
+typedef struct s_simulation
+{
+	int				death_flag;
+	pthread_mutex_t	death_flag_lock;
+	long long		simulation_start;
+	pthread_mutex_t	logprint_lock;
+
+}					t_simulation;
+
+typedef struct s_fork
+{
+	pthread_mutex_t	fork_lock;
+}					t_fork;
+
+typedef struct s_philo
+{
+	int				philo_id;
+	pthread_t		thread_philo;
+	t_arguments		*philo_arguments;
+	t_fork			*right_fork;
+	t_fork			*left_fork;
+	long long		last_meal_time;
+	int				meals_eaten;
+	t_simulation	*simulation_state;
+}					t_philo;
+
+int					parsing(int argc, char **argv);
+int					check_input(char *argv);
+int					my_atoi(const char *nptr);
+int					my_isdigit(int c);
+void	init_program_arguments(char **argv);
 #endif
